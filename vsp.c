41ba43c0e941f9a5aa68573a930b2f17adbe540f
Vinayaka <spvinayaka901@gmail.com>
2024-12-23 23:52:56 +0530

    Update vsp.c

--- vsp.c
+++ vsp.c
@@ -1,69 +1,69 @@
-#include<stdio.h>
-#include<time.h>
-void merge(int arr[],int left,int mid,int right){
-	int n1=mid-left+1;
-	int n2=right-mid;
-	int leftarr[n1],rightarr[n2];
-	for(int i=0;i<n1;i++){
-		leftarr[i]=leftarr[left+i];
-	}
-	for(int j=0;j<n2;j++){
-		rightarr[j]=rightarr[mid=+1+j];
-	}
-	int i=0,j=0,k=left;
-	while(i<n1 &&j<n2){
-		if(leftarr[i]<=rightarr[j]){
-			arr[k]=leftarr[i];
-			i++;
-		}
-		else
-		{
-			arr[k]=rightarr[j];
-			j++;
-		}
-		k++;
-	}
-	while(i<n1){
-		arr[k]=leftarr[i];
-		i++;
-		k++;
-	}
-	while(j<n2){
-		arr[k]=rightarr[j];
-		j++;
-		k++;
-	}
-}
-void mergesort(int a[],int left,int right){
-	if(left<right){
-		int mid=left+(right-left)/2;
-		mergesort(a,left,mid);
-		mergesort(a,mid+1,right);
-		merge(a,left,mid,right);
-	}
+#include <stdio.h>
+
+#define MAX_VERTICES 10
+
+int parent[MAX_VERTICES], n, cost[MAX_VERTICES][MAX_VERTICES];
+
+int find(int i) {
+    while (parent[i] != i)
+        i = parent[i];
+    return i;
 }
 
-	
-void printArray(int a[],int n){
-	for(int i=0;i<n;i++){
-		printf("%d ",a[i]);
-	}
-	printf("\n");
+void union_sets(int i, int j) {
+    int a = find(i);
+    int b = find(j);
+    parent[a] = b;
 }
-int main(){
-	int n;
-	printf("Enter the size\n");
-	scanf("%d",&n);
-	int a[n],l=0,h=n-1;
-	printf("Enter the array elements\n");
-	for(int i=0;i<n;i++){
-		scanf("%d",&a[i]);
-	}
-	clock_t start=clock();
-	mergesort(a,l,h);
-	clock_t end=clock();
-	printArray(a,n);
-	double t=((double)(end-start)/CLOCKS_PER_SEC);
-	printf("time taken is %f sec",t);
-	return 0;
+
+void kruskal() {
+    int mincost = 0, ne = 0;
+    printf("\nEdges in the minimum spanning tree:\n");
+    
+    while (ne < n - 1) {
+        int min = 999, a = -1, b = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (find(i) != find(j) && cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+
+        if (a != -1 && b != -1) {
+            union_sets(a, b);
+            printf("Edge %d: (%d - %d) cost: %d\n", ne + 1, a, b, min);
+            mincost += min;
+            ne++;
+        }
+
+        cost[a][b] = cost[b][a] = 999;
+    }
+
+    printf("\nMinimum cost = %d\n", mincost);
+}
+
+int main() {
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Mat[%d][%d]: ", i, j);
+            scanf("%d", &cost[i][j]);
+
+            if (cost[i][j] == 0)
+                cost[i][j] = 999; // Replace 0 with a large value
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        parent[i] = i; // Initialize parent array
+
+    kruskal();
+    return 0;
 }
